e619982faf59878316c86e214d65b87adc724ac8
Adam Baird <adam.baird@gmail.com>
2018-01-09 17:31:27 -0600

    updating info to secure calls

--- update_extralife_number.php
+++ update_extralife_number.php
@@ -1,14 +1,34 @@
 <?php
+include 'secrets.php';
 header("Access-Control-Allow-Origin: *");
 // this is gross but a hack for the superteams not having json format
 /* memcache prevents clients from hitting extralife more than once every 5 seconds to prevent bans */
 $memcache = new Memcache;
 $memcache->connect('localhost', 11211) or die ("Could not connect");
-$file = file_get_contents("https://www.extra-life.org/index.cfm?fuseaction=widgets.ajaxWidgetCompileHTML&callback=jsonpCallback&language=en&teamGroup=rooster&type0=login&showheader0=false&type1=login&showheader1=false&type2=thermometer&orientation2=horizontal&currencyformat2=&showheader3=false&currencyformat3=&type3=quickstats&showtotalteams3=false&showtotalparticipants3=false&showheader4=false&type4=quickstats&showtotalteams4=false&showtotalraised4=false&showheader5=false&type5=quickstats&showtotalparticipants5=false&showtotalraised5=false&showheader6=false&type6=topFundraisers&showviewmore6=false&showheader7=false&type7=topTeams&showviewmore7=false&format=json");
-$pattern= '/data-total-raised=\\\"(.*?)\\\">/';
-preg_match($pattern,$file,$matches);
-$new_number = intval(round($matches[1]));
-$old_number = $memcache->get('donation_number');
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.donordrive.com/extralife/export/RT2017totals.json",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "donordrive-email: ".$DDemail,
+    "donordrive-password: ".$DDpw,
+    "postman-token: d58ff0dc-33fb-d65b-eafd-70b8bf4d8586"
+  ),
+));
+
+$response = json_decode(curl_exec($curl));
+$err = curl_error($curl);
+curl_close($curl);
+$new_number = round($response->result[0]->totalraised);
+$old_number = $memcache->get('donation_number_api');
 
 echo "old number: $old_number \t new number: $new_number \n\n";
 
@@ -18,4 +38,5 @@ if ($new_number > $old_number){
         $data = ARRAY("amount-raised" => $new_number);
         echo json_encode($data);
 }
+
 ?>
